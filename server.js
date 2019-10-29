4e87df11748de88573291d89382c7f3338389c9e
Jim Schlader <jamesschlader@yahoo.com>
2019-10-29 17:09:04 -0600

    added functionality that can handle a query string in the url, not just url-encoded parameter

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const wholeEnchilada = require("./parsers/wholeString");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  console.log(req.query);
+  req.query.csvStuff
+    ? res.send(wholeEnchilada(req.query.csvStuff).join("\n"))
+    : res.send(
+        "Add either a query param called csvStuff or a urlencoded string to the url to parse."
+      );
+});
+
 app.get("/:csvStuff", (req, res) => {
   res.send(wholeEnchilada(req.params.csvStuff).join("\n"));
 });

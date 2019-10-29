b9a605eb3d6bdb2c6a134743b87263d57e9f0c71
Jim Schlader <jamesschlader@yahoo.com>
2019-10-29 14:02:57 -0600

    formatted the return of the status strings to exactly match spec

--- parsers/singleLine.js
+++ parsers/singleLine.js
@@ -10,8 +10,8 @@ const target = text => {
   return result.length === 7
     ? `[${result[0]}, ${result[1]}][${result[2]}][${result[3]}][${result[4]}][${
         result[5]
-      }, ${result[6]}]`
-    : result2.join();
+      },${result[6]}]`
+    : result2.join("");
 };
 
 module.exports = target;
